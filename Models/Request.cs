18ba03817125588eb356da31fd019d4d03dd8caa
SebastianPagacz <234595@student.uek.krakow.pl>
2025-07-17 15:19:41 +0200

    Add project files.

---
+++ Models/Request.cs
@@ -0,0 +1,7 @@
+﻿namespace GitHubReposCMD.Models;
+
+public class Request
+{
+    public string Name { get; set; } = string.Empty;
+    public int Forks_Count { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using GitHubReposCMD.Models;
+using Newtonsoft.Json;
+
+namespace GitHubReposCMD;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Type in github username: ");
+        var username = Console.ReadLine();
+        string url = $"https://api.github.com/users/{username}/repos";
+        using (HttpClient client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+
+            var result = await client.GetAsync(url);
+            var json = await result.Content.ReadAsStringAsync();
+            var deserializedData = JsonConvert.DeserializeObject<List<Request>>(json);
+            var finalData = deserializedData.Where(d => d.Forks_Count == 0).ToList();
+
+            for (int i = 0; i < finalData.Count; i++) 
+            {
+                Console.WriteLine($"{finalData[i].Name} : {finalData[i].Forks_Count}");
+            }
+        }
+    }
+}
